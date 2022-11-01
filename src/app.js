d220a9415db7131e6ee351f6d03cd7279add5a6f
Kaustubh Tupe <ktupe@delaplex.com>
2022-11-01 14:12:16 +0530

    remove dummy data

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const store = configureStore();
+ 
+const jsx =(
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+createRoot(document.getElementById('app')).render(jsx);
+
